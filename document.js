a01aea7e2678d27d5c61bfc68ad773adc3c4d39c
Justine <justine-lambert@hotmail.fr>
2023-05-09 23:34:24 +0200

    Ajout d'une fonction pour arreter la musique en background pour ne jouer qu'un fichier audio a la fois

--- document.js
+++ document.js
@@ -1,8 +1,19 @@
+const stopMusicBG = () => {
+    //ici document = offscreen.html ?
+    let allAudios = document.querySelectorAll("audio");
+    allAudios.forEach(function(audio) {
+        if(audio.duration > 0 && !audio.paused) {
+            audio.pause();
+        }
+    });
+}
+
 // Listen for messages
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if (msg.name === "playTrack"){
         let audioElement = document.getElementById(msg.idPlayer);
         if (audioElement.paused) {
+            stopMusicBG();
             audioElement.play();
         } else {
             audioElement.pause();
